f712cf588d232f6a32dcecca4c9355bc556087a6
Sanjay Bohara <sanjayboharasbbb@gmail.com>
2023-08-02 22:58:31 +0530

    QRCode

---
+++ QRCodeGenerator/script.js
@@ -0,0 +1,37 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector("#qrCode");
+const qrText = document.querySelector(".qe-text");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+
+dark.addEventListener("input",handleDarkColor);
+light.addEventListener("input",handleLightColor);
+qrText.addEventListener("input",handleQRText);
+sizes.addEventListener("change",handleSize);
+shareBtn.addEventListener("input",handleShare);
+
+const defaultUrl = "https://youtube.com/@AsmrProg";
+let colorLight = "#fff",
+    colorDark = "000",
+    text = defaultUrl,
+    size = 300;
+
+function handleDarkColor(e){
+    colorDark = e.target.value;
+    generateQRCode();
+}
+function handleLightColor(e){
+    colorLight = e.target.value;
+    generateQRCode();
+}
+
+function handleQRText(e){
+    const value = e.target.value;
+    text= value;
+    if(!value) {
+        text = defaultUrl;
+    }
+    generateQRCode();
+}
